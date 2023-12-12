b6f5f85d5701a299d7fbf8ab9acb37ffced53626
Alan Hattom <a.hattom@hotmail.com>
2023-12-12 22:56:15 +1100

    added fucntioning server and some login fucntion

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,45 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
+import { getAuthUri, handleCallback } from './service.js'
+import config from '../config.json'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.json())
 app.use(morgan(':method :url :status'))
+
+/***************************************************************
+                       User Auth Functions
+***************************************************************/
+
+app.get('/authUri', (req, res) => {
+  getAuthUri(req)
+    .then(authUri => res.send(authUri))
+    .catch(error => res.status(500).json({ error: error.message }))
+})
+
+app.get('/callback', (req, res) => {
+  handleCallback(req)
+    .then(token => res.json({ token }))
+    .catch(error => res.status(500).json({ error: error.message }))
+})
+
+/***************************************************************
+                       Running Server
+***************************************************************/
+app.get('/', (req, res) => res.redirect('/docs'))
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+const port = config.BACKEND_PORT
+const server = app.listen(port, () => {
+  console.log(`Backend is now listening on port ${port}!`)
+  console.log(`For API docs, navigate to http://localhost:${port}`)
+})
+
+export default server
