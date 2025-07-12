b51e974f555b70d09344c8ace675778dfdc3b1dc
Alan Hattom <a.hattom@hotmail.com>
2025-07-13 01:32:06 +1000

    connecting to new db (railway)

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,15 +1,10 @@
 import pg from 'pg';
 
 const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    ssl: {
-        rejectUnauthorized: false
-    }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
