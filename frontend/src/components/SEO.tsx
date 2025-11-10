cae32fe676baea3fd1ef5293ee4018bf63187ca2
alann-h <a.hattom@hotmail.com>
2025-11-11 07:16:52 +1100

    small seo fixes

--- frontend/src/components/SEO.tsx
+++ frontend/src/components/SEO.tsx
@@ -7,15 +7,46 @@ interface SEOProps {
   canonicalUrl: string;
   keywords?: string;
   structuredData?: object;
+  ogImage?: string;
+  ogType?: string;
+  siteName?: string;
 }
 
-const SEO: React.FC<SEOProps> = ({ title, description, canonicalUrl, keywords, structuredData }) => {
+const SEO: React.FC<SEOProps> = ({ 
+  title, 
+  description, 
+  canonicalUrl, 
+  keywords, 
+  structuredData,
+  ogImage = 'https://smartpicker.com.au/SP.png',
+  ogType = 'website',
+  siteName = 'Smart Picker'
+}) => {
   return (
     <Helmet>
+      {/* Basic Meta Tags */}
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={canonicalUrl} />
       {keywords && <meta name="keywords" content={keywords} />}
+      
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content={ogType} />
+      <meta property="og:url" content={canonicalUrl} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImage} />
+      <meta property="og:site_name" content={siteName} />
+      <meta property="og:locale" content="en_AU" />
+      
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content={canonicalUrl} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={ogImage} />
+      
+      {/* Structured Data */}
       {structuredData && (
         <script type="application/ld+json">
           {JSON.stringify(structuredData)}
